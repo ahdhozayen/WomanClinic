3fc81a394a20bcb2b4aabdc45ba78a317cd349f7
aahd hozayen <ahd.hozayen@mashreqarabia.com>
2021-02-26 20:25:26 +0200

    Update settings.py

--- womanClinic/settings.py
+++ womanClinic/settings.py
@@ -79,13 +79,26 @@ WSGI_APPLICATION = 'womanClinic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'woman_gyno',
+        'USER': 'ahd_sysadmin',
+        'PASSWORD': 'M@$hreq123',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
