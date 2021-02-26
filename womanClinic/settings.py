aeace88e0022d598a6e188ac3ce33e78fe7faca9
aahd hozayen <ahd.hozayen@mashreqarabia.com>
2021-02-26 20:27:46 +0200

    Update settings.py

--- womanClinic/settings.py
+++ womanClinic/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'woman_gyno',
         'USER': 'ahd_sysadmin',
-        'PASSWORD': 'M@$hreq123',
+        'PASSWORD': 'm@$hreq123',
         'HOST': 'localhost',
         'PORT': '',
     }
